cb32f9138db0fa14f8382ad5b9d908d5f0405e27
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-04 04:07:19 -0300

    adicionando def cadastro_usuario e def login

--- arte_ae/views.py
+++ arte_ae/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from arte_ae.forms import UserModelForm
 
 # Create your views here.
 def index(req):
   return render(req, 'index.html')
+
+def login(req):
+  return render(req, 'login.html')
+
+def cadastro_usuario(req):
+  form = UserModelForm(req.POST or None)
+
+  if form.is_valid():
+    form.save()
+    context = {
+      'msg': 'Cadastrado com sucesso'
+    }
+    return render(req, 'cadastro-usuario.html', context)
+  context = {
+    'form': form
+  }
+  return render(req, 'cadastro-usuario.html', context)
