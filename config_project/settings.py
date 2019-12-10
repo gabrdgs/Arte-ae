c8da1866876e430b19d7694299b692c231d11e62
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-10 05:28:34 -0300

    add MEDIA_ROOT e MEDIA_URL

--- config_project/settings.py
+++ config_project/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_URL = '/login'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+LOGIN_URL = '/login'
