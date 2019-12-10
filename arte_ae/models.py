87e117f7760f2d0cf381967ed88a68392ff2f3c4
MauricioDantas <mauriciodantas.contato01@bol.com.br>
2019-12-10 05:24:24 -0300

    add campos de horario, data e foreignKey de usuarios

--- arte_ae/models.py
+++ arte_ae/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Evento(models.Model):
-    cep = models.CharField(max_length=50)
+    nome = models.CharField(max_length=300)
     logradouro = models.CharField(max_length=300)
+    numero = models.CharField(max_length=4)
+    cep = models.CharField(max_length=50)
     bairro = models.CharField(max_length=300)
     localidade = models.CharField(max_length=300)
     uf = models.CharField(max_length=2)
-    numero = models.CharField(max_length=4)
-    dataHora = models.DateField()
+    data = models.DateField()
+    horario = models.TimeField()
     imagem = models.ImageField()
-    nome = models.CharField(max_length=300)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
