36ffc631f41bc8b5e167972e8c8bd1f5a593350a
gabrdgs <gabbiematiias@gmail.com>
2019-12-11 13:02:07 -0300

    Editar settings.py para subir no heroku

--- config_project/settings.py
+++ config_project/settings.py
@@ -124,6 +124,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = 'media/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 LOGIN_URL = '/login'
 
-'django_heroku.settings(locals())'
+django_heroku.settings(locals())
