e9bbd62a1d4566e674ec36e10287d69184da673f
gabrdgs <gabbiematiias@gmail.com>
2019-12-11 15:25:13 -0300

    deletar staticfiles

--- config_project/settings.py
+++ config_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,13 +121,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = 'media/'
 
 LOGIN_URL = '/login'
-
-django_heroku.settings(locals())
